f43c8cfac7c0439afebf9a38ce5890fcdbfeb653
itamm15 <osinski.mateusz15@gmail.com>
2024-05-13 16:49:14 +0200

    chore: create Layout page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Menu } from './components/Menu';
 import { Footer } from './components/Footer';
 import { HomePage } from "./components/HomePage";
 import { SpecialistsPage } from "./components/SpecialistsPage";
 import { NotFound } from "./components/NotFound";
+import { Layout } from "./components/Layout";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Menu />} >
+        <Route path="/" element={<Layout />} >
           <Route index element={<HomePage />} />
           <Route path="/doctors" element={<SpecialistsPage />} />
           <Route path="*" element={<NotFound />} />
