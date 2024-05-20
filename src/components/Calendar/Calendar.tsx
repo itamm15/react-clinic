4e17bfb03d386936c03345ce102c7fb96fc1823f
itamm15 <osinski.mateusz15@gmail.com>
2024-05-21 00:34:15 +0200

    chore: add events to Calendar:

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -1,3 +1,26 @@
+import FullCalendar from "@fullcalendar/react";
+import listPlugin from "@fullcalendar/list";
+import { DOCTOR_APPOINTMENTS } from "./consts";
+import { EventSourceInput } from "@fullcalendar/core";
+import "./Calendar.css";
+
 export function Calendar() {
-  return <>hello</>;
+  const doctorAppointmentEvents: EventSourceInput = DOCTOR_APPOINTMENTS.map(
+    (appointment) => ({
+      id: appointment.id.toString(),
+      title: appointment.person + ", " + appointment.title,
+      start: appointment.date,
+    }),
+  );
+
+  return (
+    <div className="d-flex justify-content-center align-items-center calendar-container">
+      <FullCalendar
+        plugins={[listPlugin]}
+        initialView="listWeek"
+        events={doctorAppointmentEvents}
+        height={"auto"}
+      />
+    </div>
+  );
 }
---
+++ src/components/Calendar/consts.ts
@@ -0,0 +1,189 @@
+type DoctorAppointment = {
+  id: number;
+  title: string;
+  person: string;
+  date: string;
+};
+
+export const DOCTOR_APPOINTMENTS: DoctorAppointment[] = [
+  {
+    id: 1,
+    title: "Kardiolog",
+    person: "Dr. Krzysztof Zjadek",
+    date: "2024-05-01 10:00",
+  },
+  {
+    id: 2,
+    title: "Kardiolog",
+    person: "Dr. Krzysztof Zjadek",
+    date: "2024-05-08 10:00",
+  },
+  {
+    id: 3,
+    title: "Kardiolog",
+    person: "Dr. Krzysztof Zjadek",
+    date: "2024-05-15 10:00",
+  },
+  {
+    id: 4,
+    title: "Kardiolog",
+    person: "Dr. Krzysztof Zjadek",
+    date: "2024-05-22 10:00",
+  },
+  {
+    id: 5,
+    title: "Kardiolog",
+    person: "Dr. Krzysztof Zjadek",
+    date: "2024-05-29 10:00",
+  },
+  {
+    id: 6,
+    title: "Neurolog",
+    person: "Dr. Karina Wojska",
+    date: "2024-05-02 09:00",
+  },
+  {
+    id: 7,
+    title: "Neurolog",
+    person: "Dr. Karina Wojska",
+    date: "2024-05-09 09:00",
+  },
+  {
+    id: 8,
+    title: "Neurolog",
+    person: "Dr. Karina Wojska",
+    date: "2024-05-16 09:00",
+  },
+  {
+    id: 9,
+    title: "Neurolog",
+    person: "Dr. Karina Wojska",
+    date: "2024-05-23 09:00",
+  },
+  {
+    id: 10,
+    title: "Neurolog",
+    person: "Dr. Karina Wojska",
+    date: "2024-05-30 09:00",
+  },
+  {
+    id: 11,
+    title: "Ortopeda",
+    person: "Dr. Karol Poss",
+    date: "2024-05-03 12:30",
+  },
+  {
+    id: 12,
+    title: "Ortopeda",
+    person: "Dr. Karol Poss",
+    date: "2024-05-10 12:30",
+  },
+  {
+    id: 13,
+    title: "Ortopeda",
+    person: "Dr. Karol Poss",
+    date: "2024-05-17 12:30",
+  },
+  {
+    id: 14,
+    title: "Ortopeda",
+    person: "Dr. Karol Poss",
+    date: "2024-05-24 12:30",
+  },
+  {
+    id: 15,
+    title: "Ortopeda",
+    person: "Dr. Karol Poss",
+    date: "2024-05-31 12:30",
+  },
+  {
+    id: 16,
+    title: "Stomatolog",
+    person: "Dr. Karol Wojteka",
+    date: "2024-05-06 10:00",
+  },
+  {
+    id: 17,
+    title: "Stomatolog",
+    person: "Dr. Karol Wojteka",
+    date: "2024-05-13 10:00",
+  },
+  {
+    id: 18,
+    title: "Stomatolog",
+    person: "Dr. Karol Wojteka",
+    date: "2024-05-20 10:00",
+  },
+  {
+    id: 19,
+    title: "Stomatolog",
+    person: "Dr. Karol Wojteka",
+    date: "2024-05-27 10:00",
+  },
+  {
+    id: 20,
+    title: "Diabetolog",
+    person: "Dr. Magdalena Kowalska",
+    date: "2024-05-07 14:00",
+  },
+  {
+    id: 21,
+    title: "Diabetolog",
+    person: "Dr. Magdalena Kowalska",
+    date: "2024-05-14 14:00",
+  },
+  {
+    id: 22,
+    title: "Diabetolog",
+    person: "Dr. Magdalena Kowalska",
+    date: "2024-05-21 14:00",
+  },
+  {
+    id: 23,
+    title: "Diabetolog",
+    person: "Dr. Magdalena Kowalska",
+    date: "2024-05-28 14:00",
+  },
+  {
+    id: 24,
+    title: "Pediatra",
+    person: "Dr. Szymon Szczepała",
+    date: "2024-05-06 11:00",
+  },
+  {
+    id: 25,
+    title: "Pediatra",
+    person: "Dr. Szymon Szczepała",
+    date: "2024-05-13 11:00",
+  },
+  {
+    id: 26,
+    title: "Pediatra",
+    person: "Dr. Szymon Szczepała",
+    date: "2024-05-20 11:00",
+  },
+  {
+    id: 27,
+    title: "Pediatra",
+    person: "Dr. Szymon Szczepała",
+    date: "2024-05-27 11:00",
+  },
+  {
+    id: 28,
+    title: "Pediatra",
+    person: "Dr. Szymon Szczepała",
+    date: "2024-06-03 11:00",
+  },
+  {
+    id: 29,
+    title: "Kardiolog",
+    person: "Dr. Krzysztof Zjadek",
+    date: "2024-06-05 10:00",
+  },
+  {
+    id: 30,
+    title: "Neurolog",
+    person: "Dr. Karina Wojska",
+    date: "2024-06-06 09:00",
+  },
+];
