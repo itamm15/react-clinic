cf690883cdcf18c7b0126ff093370a1e7b9ef759
itamm15 <osinski.mateusz15@gmail.com>
2024-05-23 23:31:01 +0200

    chore: use simple descriptors instead of complex card

--- src/components/ClinicDescriptionCard/ClinicDescriptionCard.tsx
+++ src/components/ClinicDescriptionCard/ClinicDescriptionCard.tsx
@@ -1,50 +1,49 @@
-import { Col, Button } from "react-bootstrap";
-import doctorhomepage2 from "../../assets/doctorhomepage2.jpg";
+import { Col, Card } from "react-bootstrap";
+import { FaCalendarCheck, FaSearch } from "react-icons/fa";
+import { IoTime } from "react-icons/io5";
+import './ClinicDescriptionCard.css';
+
+const DOCTORS = [
+  {
+    title: "Znajdź specjalistę",
+    text: "Wybierz spośród wielu dostępnych specjalistów i lekarzy. Przeglądaj opinie innych pacjentów i wybieraj!",
+    icon: <FaSearch style={{ width: "20px", height: "20px" }} />,
+  },
+  {
+    title: "Łatwo umów wizytę",
+    text: "Wybierz odpowiadający Ci termin, podaj swoje dane i otrzymaj pełny dostęp do usług. Bardzo szybki i łatwy proces!",
+    icon: <FaCalendarCheck style={{ width: "20px", height: "20px" }} />,
+  },
+  {
+    title: "Przyjdź na wizytę",
+    text: "O wybranym terminie przypominimy Ci automatycznie za pomocą wiadomości SMS i mailem.",
+    icon: <IoTime style={{ width: "20px", height: "20px" }} />,
+  },
+]
 
 export function ClinicDescriptionCard() {
   return (
-    <div className="przychodnia-desc d-flex">
-      <Col className="przychodnia-description ms-5">
-        <h2 className="text-center">Przychodnia lekarska Świtałka</h2>
-        <h4>Zadbaj o swoje zdrowie w naszej przychodni medycznej! </h4>
-        <h4>
-          Nasz doświadczony zespół lekarzy jest gotowy, by zadbać o Ciebie i
-          Twoją rodzinę. Zapraszamy do skorzystania z naszych usług medycznych
-          już dziś!{" "}
-        </h4>
-        <h4>
-          Oferujemy kompleksową opiekę medyczną dla całej rodziny, w tym:{" "}
-        </h4>
-        <ul className="ps-3">
-          <li>
-            <h4>Poradnictwo zdrowotne</h4>
-          </li>
-          <li>
-            <h4>Badania profilaktyczne</h4>
-          </li>
-          <li>
-            <h4>Konsultacje lekarskie</h4>
-          </li>
-          <li>
-            <h4>Leczenie chorób przewlekłych</h4>
-          </li>
-          <li>
-            <h4>Rehabilitację</h4>
-          </li>
-        </ul>
-        <Button variant="light" href="/services">
-          Więcej
-        </Button>
-      </Col>
-      <Col>
-        <div className="image-container">
-          <img
-            src={doctorhomepage2}
-            alt="lekarz z tabletem"
-            className="image"
-          />
-        </div>
-      </Col>
+    <div className="przychodnia-desc d-flex justify-content-evenly align-items-stretch flex-wrap">
+        {DOCTORS.map((doctor) => (
+            <Col
+                key={doctor.title}
+                xs={12} sm={6} md={4} lg={3}
+                className="d-flex align-items-stretch mb-3"
+            >
+                <Card className="w-100">
+                    <Card.Body className="d-flex flex-column">
+                        <div className="d-flex align-items-center mb-2">
+                            <div>
+                                <Card.Title>{doctor.icon} {doctor.title}</Card.Title>
+                                <Card.Text>
+                                    {doctor.text}
+                                </Card.Text>
+                            </div>
+                        </div>
+                    </Card.Body>
+                </Card>
+            </Col>
+        ))}
     </div>
   );
 }
