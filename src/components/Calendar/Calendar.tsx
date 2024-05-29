52d69ed596b8bff2b9d5def9fbbdc4d860b4bd56
itamm15 <osinski.mateusz15@gmail.com>
2024-05-29 23:57:44 +0200

    chore: add modal to book appointment on event click

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -1,10 +1,14 @@
 import FullCalendar from "@fullcalendar/react";
 import listPlugin from "@fullcalendar/list";
 import { DOCTOR_APPOINTMENTS } from "./consts";
-import { EventSourceInput } from "@fullcalendar/core";
+import { EventClickArg, EventSourceInput } from "@fullcalendar/core";
 import "./Calendar.css";
+import { Dispatch, SyntheticEvent, useState } from "react";
+import { Button, Modal } from "react-bootstrap";
 
 export function Calendar() {
+  const [showModal, setShowModal] = useState<boolean>(false);
+
   const doctorAppointmentEvents: EventSourceInput = DOCTOR_APPOINTMENTS.map(
     (appointment) => ({
       id: appointment.id.toString(),
@@ -23,13 +27,27 @@ export function Calendar() {
         locale={"pl"}
         eventColor="#198754"
         noEventsText="Brak wizyt, proszę spróbowac ponownie później"
-        // eventClick={onClickEvent}
+        eventClick={onClickCalendarEvent}
       />
+
+
+      <Modal show={showModal} onHide={(() => setShowModal(false))}>
+        <Modal.Header closeButton>
+          <Modal.Title>Rezerwacja wizyty</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p>Rezerwacja wizyty</p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="success">
+            Zarezerwuj wizytę
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 
-  // TODO add event click
-  // function onClickEvent(event) {
-  //   // show dialog
-  // }
+  function onClickCalendarEvent(event: EventClickArg) {
+    setShowModal(true);
+  }
 }
