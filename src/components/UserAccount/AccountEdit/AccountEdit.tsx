06a91b005bf85e72736e30cca5d79128c848a592
itamm15 <osinski.mateusz15@gmail.com>
2024-06-14 22:59:53 +0200

    chore: add basic validation for accountedit

--- src/components/UserAccount/AccountEdit/AccountEdit.tsx
+++ src/components/UserAccount/AccountEdit/AccountEdit.tsx
@@ -1,57 +1,84 @@
+import React from "react";
 import { Button, Form } from "react-bootstrap";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from "yup";
 import "./AccountEdit.css";
 
+type FormData = {
+  username: string;
+  surname: string;
+  email: string;
+  phone: string;
+  address: string;
+};
+
+const schema = yup.object({
+  username: yup.string().required("Imię jest wymagane"),
+  surname: yup.string().required("Nazwisko jest wymagane"),
+  email: yup.string().email("Podaj prawidłowy adres email").required("Email jest wymagany"),
+  phone: yup.string().matches(/^[0-9]{9}$/, "Podaj prawidłowy numer telefonu składający się z 9 cyfr").required("Numer telefonu jest wymagany"),
+  address: yup.string().required("Adres zamieszkania jest wymagany"),
+}).required();
+
 export function AccountEdit() {
+  const { register, handleSubmit, formState: { errors } } = useForm<FormData>({
+    resolver: yupResolver(schema)
+  });
+
+  const onSubmit: SubmitHandler<FormData> = data => {
+    console.log(data);
+  };
+
   return (
     <div className="d-flex justify-content-center align-items-center flex-column mt-4">
       <h2>Zmień swoje dane</h2>
       <div className="w-50">
-        <Form>
+        <Form onSubmit={handleSubmit(onSubmit)}>
           <Form.Group controlId="name">
             <Form.Label>Imię:</Form.Label>
-            <Form.Control type="text" name="username" value="Janusz" required />
+            <Form.Control
+              type="text"
+              {...register("username")}
+            />
+            {errors.username && <p className="error-message">{errors.username.message}</p>}
           </Form.Group>
 
-          <Form.Group controlId="nazwisko">
+          <Form.Group controlId="surname">
             <Form.Label>Nazwisko</Form.Label>
             <Form.Control
               type="text"
-              name="username"
-              value="Januszowski"
-              required
+              {...register("surname")}
             />
+            {errors.surname && <p className="error-message">{errors.surname.message}</p>}
           </Form.Group>
 
           <Form.Group controlId="email">
             <Form.Label>Email:</Form.Label>
             <Form.Control
               type="email"
-              name="email"
-              value="janusz.januszowski@januszowo.pl"
-              required
+              {...register("email")}
             />
+            {errors.email && <p className="error-message">{errors.email.message}</p>}
           </Form.Group>
 
           <Form.Group controlId="phone">
             <Form.Label>Numer telefonu:</Form.Label>
             <Form.Control
               type="tel"
-              name="phone"
-              value="512 121 122"
-              pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
-              required
+              {...register("phone")}
             />
+            {errors.phone && <p className="error-message">{errors.phone.message}</p>}
           </Form.Group>
 
           <Form.Group controlId="address">
-            <Form.Label>Addres zamieszkania:</Form.Label>
+            <Form.Label>Adres zamieszkania:</Form.Label>
             <Form.Control
               as="textarea"
-              name="address"
               rows={4}
-              value="Zakopane, ul. Jana Pawła II, 22"
-              required
+              {...register("address")}
             />
+            {errors.address && <p className="error-message">{errors.address.message}</p>}
           </Form.Group>
 
           <Button variant="success" type="submit" className="mt-2 mb-2">
