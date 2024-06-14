1d2dd20114f303e9e56b33b7c5445f83c725a566
itamm15 <osinski.mateusz15@gmail.com>
2024-06-14 23:12:23 +0200

    chore: create user context

---
+++ src/contexts/UserContext.tsx
@@ -0,0 +1,33 @@
+import { createContext, useState, useContext, ReactNode } from 'react';
+
+type User = {
+  id: number;
+  name: string;
+  email: string;
+  role: string;
+}
+
+type UserContextProps = {
+  user: User | null;
+  setUser: (user: User | null) => void;
+}
+
+const UserContext = createContext<UserContextProps | undefined>(undefined);
+
+export const UserProvider = ({ children }: { children: ReactNode }) => {
+  const [user, setUser] = useState<User | null>(null);
+
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export const useUser = (): UserContextProps => {
+  const context = useContext(UserContext);
+  if (!context) {
+    throw new Error('useUser must be used within a UserProvider');
+  }
+  return context;
+};
